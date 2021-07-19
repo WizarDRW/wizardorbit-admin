c6d3222827051317b700ba457ca5bf316f0fc376
Burak <wizard1456@hotmail.com>
2021-07-19 17:33:47 +0300

    gzip

--- vue.config.js
+++ vue.config.js
@@ -32,6 +32,11 @@ module.exports = {
       }
     }
   },
+  chainWebpack: config => {
+    config.performance
+      .maxEntrypointSize(400000)
+      .maxAssetSize(400000)
+  },
   devServer: {
     headers: {
       "Access-Control-Allow-Credentials": true,
