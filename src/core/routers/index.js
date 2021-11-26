a1dae4ec39c633fd2aafa90b9235828e37b4924d
Burak <wizard1456@hotmail.com>
2021-11-26 22:45:58 +0300

    Path problems fixed in router.js

--- src/core/routers/index.js
+++ src/core/routers/index.js
@@ -44,7 +44,7 @@ export default new Router({
       path: '/login',
       query: { returnPath: '' },
       name: 'Login',
-      component: () => import('./views/auth/Login.vue')
+      component: () => import('@/views/auth/Login.vue')
     },
     {
       path: '/',
@@ -52,13 +52,13 @@ export default new Router({
       meta: { lang: 'router.home' },
       redirect: '/dashboard',
       beforeEnter: isAuth,
-      component: () => import("./layouts/Container.vue"),
+      component: () => import("@/layouts/Container.vue"),
       children: [
         {
           path: '/dashboard',
           name: 'Dashboard',
           beforeEnter: (to, from, next) => isAuth(to, from, next),
-          component: () => import('./views/dashboard/Index.vue')
+          component: () => import('@/views/dashboard/Index.vue')
         },
         {
           path: '/chapter',
@@ -75,21 +75,21 @@ export default new Router({
               name: 'NewChapter',
               meta: { lang: 'router.user.chapter.new' },
               beforeEnter: (to, from, next) => isAuth(to, from, next),
-              component: () => import("./views/client/chapters/Create.vue")
+              component: () => import("@/views/client/chapters/Create.vue")
             },
             {
               path: '/my-chapters',
               name: 'MyChapters',
               meta: { lang: 'router.user.chapter.my' },
               beforeEnter: (to, from, next) => isAuth(to, from, next),
-              component: () => import("./views/client/chapters/List.vue")
+              component: () => import("@/views/client/chapters/List.vue")
             },
             {
               path: '/chapter/edit/:id',
               name: 'EditChapter',
               meta: { lang: 'router.user.chapter.edit' },
               beforeEnter: (to, from, next) => isAuth(to, from, next),
-              component: () => import("./views/client/chapters/Edit.vue"),
+              component: () => import("@/views/client/chapters/Edit.vue"),
             }
           ]
         },
@@ -108,21 +108,21 @@ export default new Router({
               name: 'NewNews',
               meta: { lang: 'router.user.news.new' },
               beforeEnter: (to, from, next) => isAuth(to, from, next),
-              component: () => import("./views/client/news/Create.vue")
+              component: () => import("@/views/client/news/Create.vue")
             },
             {
               path: '/my-news',
               name: 'MyNews',
               meta: { lang: 'router.user.news.my' },
               beforeEnter: (to, from, next) => isAuth(to, from, next),
-              component: () => import("./views/client/news/List.vue")
+              component: () => import("@/views/client/news/List.vue")
             },
             {
               path: '/news/edit/:id',
               name: 'EditNews',
               meta: { lang: 'router.user.news.edit' },
               beforeEnter: (to, from, next) => isAuth(to, from, next),
-              component: () => import("./views/client/news/Edit.vue"),
+              component: () => import("@/views/client/news/Edit.vue"),
             }
           ]
         },
@@ -141,28 +141,28 @@ export default new Router({
               name: 'NewForum',
               meta: { lang: 'router.user.forum.new' },
               beforeEnter: (to, from, next) => isAuth(to, from, next),
-              component: () => import("./views/client/forums/Create.vue")
+              component: () => import("@/views/client/forums/Create.vue")
             },
             {
               path: '/my-forums',
               name: 'MyForums',
               meta: { lang: 'router.user.forum.my' },
               beforeEnter: (to, from, next) => isAuth(to, from, next),
-              component: () => import("./views/client/forums/List.vue")
+              component: () => import("@/views/client/forums/List.vue")
             },
             {
               path: '/forum/edit/:id',
               name: 'EditForum',
               meta: { lang: 'router.user.forum.edit' },
               beforeEnter: (to, from, next) => isAuth(to, from, next),
-              component: () => import("./views/client/forums/Edit.vue"),
+              component: () => import("@/views/client/forums/Edit.vue"),
             },
             {
               path: '/forum/detail/:id',
               name: 'DetailForum',
               meta: { lang: 'router.user.forum.detail' },
               beforeEnter: (to, from, next) => isAuth(to, from, next),
-              component: () => import("./views/client/forums/Detail.vue")
+              component: () => import("@/views/client/forums/Detail.vue")
             }
           ]
         },
@@ -181,28 +181,28 @@ export default new Router({
               name: 'BookShelves',
               meta: { lang: 'router.user.library.my' },
               beforeEnter: (to, from, next) => isAuth(to, from, next),
-              component: () => import("./views/libraries/List.vue")
+              component: () => import("@/views/libraries/List.vue")
             },
             {
               path: '/library/new-book',
               name: 'NewBook',
               meta: { lang: 'router.user.library.new' },
               beforeEnter: (to, from, next) => isAuth(to, from, next),
-              component: () => import("./views/libraries/Create.vue")
+              component: () => import("@/views/libraries/Create.vue")
             },
             {
               path: '/library/content/:id',
               name: 'LibraryContent',
               meta: { lang: 'router.user.library.detail' },
               beforeEnter: (to, from, next) => isAuth(to, from, next),
-              component: () => import("./views/libraries/Content.vue")
+              component: () => import("@/views/libraries/Content.vue")
             },
             {
               path: '/library/edit/:id',
               name: 'LibraryEdit',
               meta: { lang: 'router.user.library.edit' },
               beforeEnter: (to, from, next) => isAuth(to, from, next),
-              component: () => import("./views/libraries/Edit.vue")
+              component: () => import("@/views/libraries/Edit.vue")
             },
           ]
         },
@@ -210,13 +210,13 @@ export default new Router({
           path: '/drafts',
           name: 'Draft',
           meta: { lang: 'router.user.draft' },
-          component: () => import(`./views/drafts/List.vue`)
+          component: () => import(`@/views/drafts/List.vue`)
         },
         {
           path: '/useroptions',
           name: 'UserOption',
           meta: { lang: 'router.user.userOption' },
-          component: () => import(`./views/profile/ProfileOption.vue`)
+          component: () => import(`@/views/profile/ProfileOption.vue`)
         },
         {
           path: '/admin/chapter',
@@ -236,21 +236,21 @@ export default new Router({
               name: 'AdminNewChapter',
               meta: { lang: 'router.admin.chapter.new' },
               beforeEnter: (to, from, next) => isAuth(to, from, next),
-              component: () => import("./views/admin/chapters/Create.vue")
+              component: () => import("@/views/admin/chapters/Create.vue")
             },
             {
               path: '/admin/chapters',
               name: 'AdminChapters',
               meta: { lang: 'router.admin.chapter.total' },
               beforeEnter: (to, from, next) => isAuth(to, from, next),
-              component: () => import("./views/admin/chapters/List.vue")
+              component: () => import("@/views/admin/chapters/List.vue")
             },
             {
               path: '/admin/chapter/edit/:id',
               name: 'AdminEditChapter',
               meta: { lang: 'router.admin.chapter.edit' },
               beforeEnter: (to, from, next) => isAuth(to, from, next),
-              component: () => import("./views/admin/chapters/Edit.vue"),
+              component: () => import("@/views/admin/chapters/Edit.vue"),
             }
           ]
         },
@@ -269,21 +269,21 @@ export default new Router({
               name: 'AdminNewNews',
               meta: { lang: 'router.admin.news.new' },
               beforeEnter: (to, from, next) => isAuth(to, from, next),
-              component: () => import("./views/admin/news/Create.vue")
+              component: () => import("@/views/admin/news/Create.vue")
             },
             {
               path: '/admin/news',
               name: 'AdminTheNews',
               meta: { lang: 'router.admin.news.total' },
               beforeEnter: (to, from, next) => isAuth(to, from, next),
-              component: () => import("./views/admin/news/List.vue")
+              component: () => import("@/views/admin/news/List.vue")
             },
             {
               path: '/admin/news/edit/:id',
               name: 'AdminEditNews',
               meta: { lang: 'router.admin.chapter.edit' },
               beforeEnter: (to, from, next) => isAuth(to, from, next),
-              component: () => import("./views/admin/news/Edit.vue"),
+              component: () => import("@/views/admin/news/Edit.vue"),
             }
           ]
         },
@@ -305,28 +305,28 @@ export default new Router({
               name: 'AdminNewForm',
               meta: { lang: 'router.admin.forum.new' },
               beforeEnter: (to, from, next) => isAuth(to, from, next),
-              component: () => import("./views/admin/forums/Create.vue")
+              component: () => import("@/views/admin/forums/Create.vue")
             },
             {
               path: '/admin/forums',
               name: 'AdminForms',
               meta: { lang: 'router.admin.forum.total' },
               beforeEnter: (to, from, next) => isAuth(to, from, next),
-              component: () => import("./views/admin/forums/List.vue")
+              component: () => import("@/views/admin/forums/List.vue")
             },
             {
               path: '/admin/forum/edit/:id',
               name: 'AdminEditForum',
               meta: { lang: 'router.admin.forum.edit' },
               beforeEnter: (to, from, next) => isAuth(to, from, next),
-              component: () => import("./views/admin/forums/Edit.vue"),
+              component: () => import("@/views/admin/forums/Edit.vue"),
             },
             {
               path: '/admin/forum/detail/:id',
               name: 'AdminDetailForum',
               meta: { lang: 'router.admin.forum.detail' },
               beforeEnter: (to, from, next) => isAuth(to, from, next),
-              component: () => import("./views/admin/forums/Detail.vue")
+              component: () => import("@/views/admin/forums/Detail.vue")
             }
           ]
         },
@@ -369,7 +369,7 @@ export default new Router({
                   path: "/superuser/users/edit/:id",
                   name: "UserEdit",
                   meta: { lang: 'router.superUser.users.edit' },
-                  component: () => import("./views/superuser/users/Edit")
+                  component: () => import("@/views/superuser/users/Edit")
                 },
               ],
             },
@@ -399,7 +399,7 @@ export default new Router({
                   path: "/superuser/about/edit/:id",
                   name: "AboutEdit",
                   meta: { lang: 'router.superUser.about.edit' },
-                  component: () => import("./views/superuser/about/Edit")
+                  component: () => import("@/views/superuser/about/Edit")
                 },
               ],
             },
@@ -429,7 +429,7 @@ export default new Router({
                   path: "/superuser/release/edit/:id",
                   name: "ReleaseEdit",
                   meta: { lang: 'router.superUser.release.edit' },
-                  component: () => import("./views/superuser/release/Edit")
+                  component: () => import("@/views/superuser/release/Edit")
                 },
               ],
             },
@@ -438,21 +438,21 @@ export default new Router({
               name: 'SuperUserChapterCategories',
               meta: { lang: 'router.superUser.category.chapter' },
               beforeEnter: (to, from, next) => isAuth(to, from, next),
-              component: () => import("./views/admin/chapters/Category.vue")
+              component: () => import("@/views/admin/chapters/Category.vue")
             },
             {
               path: '/superuser/category/news-categories',
               name: 'SuperUserNewsCategories',
               meta: { lang: 'router.superUser.category.news' },
               beforeEnter: (to, from, next) => isAuth(to, from, next),
-              component: () => import("./views/admin/news/Category.vue")
+              component: () => import("@/views/admin/news/Category.vue")
             },
             {
               path: '/superuser/category/forum-categories',
               name: 'SuperUserForumCategories',
               meta: { lang: 'router.superUser.category.forum' },
               beforeEnter: (to, from, next) => isAuth(to, from, next),
-              component: () => import("./views/admin/forums/Category.vue")
+              component: () => import("@/views/admin/forums/Category.vue")
             }
           ]
         },
@@ -461,17 +461,17 @@ export default new Router({
     {
       path: "/403",
       name: "Forbidden",
-      component: () => import("./views/pages/403.vue")
+      component: () => import("@/views/pages/403.vue")
     },
     {
       path: "**",
       name: "NotFound",
-      component: () => import("./views/pages/404.vue")
+      component: () => import("@/views/pages/404.vue")
     },
     {
       path: "/500",
       name: "ServerError",
-      component: () => import("./views/pages/500.vue")
+      component: () => import("@/views/pages/500.vue")
     }
   ],
   scrollBehavior: to => {
