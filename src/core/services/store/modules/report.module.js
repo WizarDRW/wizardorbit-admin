5afd7dbc9b6c016fd8d72a0f80765b395e3c75c8
Burak <wizard1456@hotmail.com>
2021-12-16 21:26:21 +0300

    Reprt basic module created.

---
+++ src/core/services/store/modules/report.module.js
@@ -0,0 +1,11 @@
+// actions
+export const CHAPTER_REPORTS = "chapterReports"
+// mutations
+const SET_CHAPTER_REPORTS = "setChapterReports"
+
+export default {
+    state: {},
+    getters: {},
+    actions: {},
+    mutations: {}
+}
